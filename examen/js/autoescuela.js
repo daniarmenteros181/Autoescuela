6eef9109c23a4c278461daf838ec9472527302bf
daniarmenteros181 <darmace2311@g.educaand.es>
2023-10-30 12:01:50 +0100

    Examen

--- examen/js/autoescuela.js
+++ examen/js/autoescuela.js
@@ -19,12 +19,17 @@ window.addEventListener("load",function(){
                         var pregAux=pregunta.cloneNode(true);
                         pregAux.getElementsByClassName("categoria")[0].innerHTML=y.examen[0].pregunta[i].categoria;
                         pregAux.getElementsByClassName("dificultad")[0].innerHTML=y.examen[0].pregunta[i].dificultad;
-                        
                         pregAux.getElementsByClassName("id")[0].innerHTML=y.examen[0].pregunta[i].id;
+
+                        pregAux.getElementsByClassName("res1")[0].innerHTML=y.examen[0].pregunta[i].respuesta[i].res1;
+                        pregAux.getElementsByClassName("res2")[0].innerHTML=y.examen[0].pregunta[i].respuesta[i].res2;
+                        pregAux.getElementsByClassName("res3")[0].innerHTML=y.examen[0].pregunta[i].respuesta[i].res3;
+
+
 
 
                       
-
+/*
 
                          // Agregar respuestas
                          var respuestaHTML = "<ul>";
@@ -34,7 +39,7 @@ window.addEventListener("load",function(){
                          }
                          respuestaHTML += "</ul>";
                          pregAux.innerHTML += respuestaHTML;
-
+*/
 
 
                         divExamen.appendChild(pregAux);
