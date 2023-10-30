9ae2dae24db594b224dc3e8441b2b3ad12403282
daniarmenteros181 <darmace2311@g.educaand.es>
2023-10-30 10:19:15 +0100

    Coger las preguntas y respuestas de los examenes

---
+++ examen/js/autoescuela.js
@@ -0,0 +1,52 @@
+window.addEventListener("load",function(){
+    var btnComenzar=document.getElementById("comenzar");
+    var divExamen=document.getElementById("examen");
+
+    btnComenzar.addEventListener("click", comenzar);
+
+    function comenzar(){
+        fetch("plantilla/pregunta.html")
+        .then(x=>x.text())
+        .then(y=>{
+            var contenedor=document.createElement("div");
+            contenedor.innerHTML=y;
+            var pregunta=contenedor.firstChild;
+            fetch("servidor/pregunta.json")
+                .then(x=>x.json())
+                .then(y=>{
+                    for(let i=0;i<y.examen[0].pregunta.length;i++)
+                    {
+                        var pregAux=pregunta.cloneNode(true);
+                        pregAux.getElementsByClassName("categoria")[0].innerHTML=y.examen[0].pregunta[i].categoria;
+                        pregAux.getElementsByClassName("dificultad")[0].innerHTML=y.examen[0].pregunta[i].dificultad;
+                        
+                        pregAux.getElementsByClassName("id")[0].innerHTML=y.examen[0].pregunta[i].id;
+
+
+                      
+
+
+                         // Agregar respuestas
+                         var respuestaHTML = "<ul>";
+                         var respuestas = y.examen[0].pregunta[i].respuesta[0];
+                         for (var key in respuestas) {
+                             respuestaHTML += "<li>" + key + ": " + respuestas[key] + "</li>";
+                         }
+                         respuestaHTML += "</ul>";
+                         pregAux.innerHTML += respuestaHTML;
+
+
+
+                        divExamen.appendChild(pregAux);
+
+
+                    }
+
+                })
+
+        })
+    }
+
+})
+
+         
