272de0d29f0ce8753c0b73c696ef4bd52de5e642
daniarmenteros181 <darmace2311@g.educaand.es>
2023-11-16 17:46:47 +0100

    Examen api bien

--- api/examenApi.php
+++ api/examenApi.php
@@ -1,17 +1,54 @@
 <?php
-/* if ($_SERVER['REQUEST_METHOD']=='POST')
-{
-
-} */
 header( "Access-Control-Allow-Origin: *" );
-
-
 require_once('../repositorio/db.php'); // Asegúrate de proporcionar la ruta correcta
 
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    // Obtener los datos del cuerpo de la solicitud
+    $data = json_decode(file_get_contents("php://input"), true);
+
+    // Verificar si los datos necesarios están presentes
+    if (isset($data['nombre']) && isset($data['descripcion'])) {
+        try {
+            // Obtener la conexión a la base de datos utilizando la clase db
+            $conexion = db::entrar();
+
+            // Preparar y ejecutar la consulta para agregar un nuevo examen
+            $query = "INSERT INTO examen (nombre, descripcion) VALUES (:nombre, :descripcion)";
+            $statement = $conexion->prepare($query);
+            $statement->bindParam(':nombre', $data['nombre']);
+            $statement->bindParam(':descripcion', $data['descripcion']);
+            $statement->execute();
+
+            // Obtener el ID del nuevo examen
+            $nuevoExamenId = $conexion->lastInsertId();
+
+            // Preparar y ejecutar la consulta para obtener el nuevo examen
+            $query = "SELECT id, nombre, descripcion FROM examen WHERE id = :id";
+            $statement = $conexion->prepare($query);
+            $statement->bindParam(':id', $nuevoExamenId);
+            $statement->execute();
+
+            // Obtener el nuevo examen
+            $nuevoExamen = $statement->fetch(PDO::FETCH_ASSOC);
+
+            // Convertir a JSON y enviar la respuesta
+            header('Content-type: application/json');
+            echo json_encode(['nuevoExamen' => $nuevoExamen]);
+
+        } catch (PDOException $e) {
+            // Manejar errores de la base de datos según tus necesidades
+            header('HTTP/1.1 500 Internal Server Error');
+            echo json_encode(['error' => 'Error en la base de datos: ' . $e->getMessage()]);
+        }
+    } else {
+        // Datos insuficientes en la solicitud
+        header('HTTP/1.0 400 Bad Request');
+        echo json_encode(['error' => 'Datos insuficientes en la solicitud']);
+    }
+} 
 
 
-
-   if ($_SERVER['REQUEST_METHOD'] == 'GET') {
+   elseif ($_SERVER['REQUEST_METHOD'] == 'GET') {
     if (isset($_GET['id_examen'])) {
         $idExamen = intval($_GET['id_examen']);
         try {
@@ -65,15 +102,8 @@ require_once('../repositorio/db.php'); // Asegúrate de proporcionar la ruta cor
             header('HTTP/1.1 500 Internal Server Error');
             echo json_encode(array('error' => 'Error en la base de datos: ' . $e->getMessage()));
         }
-    } else {
-        // Si no se proporciona el parámetro 'id_examen'
-        header('HTTP/1.0 400 Bad Request');
-        echo json_encode(array('error' => 'Se requiere el parámetro "id_examen" en la solicitud'));
     }
-} else {
-    // Método no permitido
-    echo json_encode(['error' => 'Método no permitido']);
-}  
+} 
  
 
  /*  if ($_SERVER['REQUEST_METHOD'] == 'GET') {
@@ -104,11 +134,85 @@ require_once('../repositorio/db.php'); // Asegúrate de proporcionar la ruta cor
 }   */
 
 
-/* elseif ($_SERVER['REQUEST_METHOD']=='DELETE')
-{
+elseif ($_SERVER['REQUEST_METHOD'] == 'DELETE') {
+    // Obtener el ID del examen a eliminar desde la URL
+    $id = $_GET['id'] ?? null;
 
-}
-elseif ($_SERVER['REQUEST_METHOD']=='PUT')
-{
-    
-} */
+    if ($id) {
+        try {
+            // Obtener la conexión a la base de datos utilizando la clase db
+            $conexion = db::entrar();
+
+            // Preparar y ejecutar la consulta para eliminar el examen
+            $query = "DELETE FROM examen WHERE id = :id";
+            $statement = $conexion->prepare($query);
+            $statement->bindParam(':id', $id);
+            $statement->execute();
+
+            // Verificar si se eliminó el examen
+            $filasAfectadas = $statement->rowCount();
+
+            if ($filasAfectadas > 0) {
+                // Examen eliminado con éxito
+                header('Content-type: application/json');
+                echo json_encode(['mensaje' => 'Examen eliminado con éxito']);
+            } else {
+                // No se encontró el examen con el ID proporcionado
+                header('HTTP/1.0 404 Not Found');
+                echo json_encode(['error' => 'No se encontró el examen con el ID proporcionado']);
+            }
+
+        } catch (PDOException $e) {
+            // Manejar errores de la base de datos según tus necesidades
+            header('HTTP/1.1 500 Internal Server Error');
+            echo json_encode(['error' => 'Error en la base de datos: ' . $e->getMessage()]);
+        }
+    } else {
+        // ID no proporcionado en la solicitud
+        header('HTTP/1.0 400 Bad Request');
+        echo json_encode(['error' => 'ID no proporcionado en la solicitud']);
+    }
+} 
+
+elseif ($_SERVER['REQUEST_METHOD'] == 'PUT') {
+    // Obtener datos del cuerpo de la solicitud
+    $data = json_decode(file_get_contents("php://input"), true);
+
+    // Verificar si se proporcionan datos válidos
+    if (isset($data['id']) && isset($data['nombre']) && isset($data['descripcion'])) {
+        try {
+            // Obtener la conexión a la base de datos utilizando la clase db
+            $conexion = db::entrar();
+
+            // Preparar y ejecutar la consulta para actualizar el examen
+            $query = "UPDATE examen SET nombre = :nombre, descripcion = :descripcion WHERE id = :id";
+            $statement = $conexion->prepare($query);
+            $statement->bindParam(':id', $data['id']);
+            $statement->bindParam(':nombre', $data['nombre']);
+            $statement->bindParam(':descripcion', $data['descripcion']);
+            $statement->execute();
+
+            // Verificar si se actualizó el examen
+            $filasAfectadas = $statement->rowCount();
+
+            if ($filasAfectadas > 0) {
+                // Examen actualizado con éxito
+                header('Content-type: application/json');
+                echo json_encode(['mensaje' => 'Examen actualizado con éxito']);
+            } else {
+                // No se encontró el examen con el ID proporcionado
+                header('HTTP/1.0 404 Not Found');
+                echo json_encode(['error' => 'No se encontró el examen con el ID proporcionado']);
+            }
+
+        } catch (PDOException $e) {
+            // Manejar errores de la base de datos según tus necesidades
+            header('HTTP/1.1 500 Internal Server Error');
+            echo json_encode(['error' => 'Error en la base de datos: ' . $e->getMessage()]);
+        }
+    } else {
+        // Datos insuficientes en la solicitud
+        header('HTTP/1.0 400 Bad Request');
+        echo json_encode(['error' => 'Datos insuficientes en la solicitud']);
+    }
+} 
